81fb52e06056630a98cfe3b243f1040ca602dbc2
Jamie Ruderman <jamieruderman@gmail.com>
2013-05-11 11:31:33 -0700

    Canvas rendering, snake length and stage wrapping

--- snakegame.js
+++ snakegame.js
@@ -5,16 +5,16 @@ var SnakeGame = {};
 (function(app){
 
   var config = {
-    scale: 14,
-    frameRate: 60 ,
-    direction: 'none'
+    scale: 10,
+    fps: 10,
+    direction: 'down'
     // speed: 1
   };
 
   app.start = function() {
     // start snake game
     $(document).ready(function() {
-      app.segment = new Segment();
+      app.snake = new Snake();
       app.stage = new Stage();
       app.renderer = new Renderer();
       app.controller = new Controller();
@@ -23,24 +23,63 @@ var SnakeGame = {};
   };
 
   var Stage = function() {
+
+    var body = $('body'),
+        scale = config.scale;
+
     var self = {
       // width, height in blocks
       size: [50, 50],
-      position: [0, 0],
-      el: $('.stage'),
-      name: 'stage'
+      name: 'stage',
+      el: $('<canvas />')
     };
+
+    self.init = function() {
+      body.append(self.el);
+      self.el.addClass('stage');
+      self.el.attr({
+        width: (self.size[0] * scale) + 'px',
+        height: (self.size[1] * scale) + 'px'
+      });
+      self.context = self.el[0].getContext('2d');
+    };
+
+    self.init();
 
     return self;
   };
 
-  var Segment = function() {
+  var Snake = function() {
     var self = {
       size: [1, 1],
       position: [0, 0],
-      el: $('.snake'),
-      name: 'segment'
+      segments: [],
+      length: 10,
+      name: 'snake'
     };
+
+    self.init = function() {
+      self.addSegment(self.position);
+    };
+
+    self.advance = function() {
+      self.addSegment();
+      if (self.segments.length > self.length) {
+        self.segments.pop();
+      }
+    };
+
+    self.addSegment = function() {
+      self.segments.unshift([self.position[0], self.position[1]]);
+    };
+
+    self.eachSegment = function(callback) {
+      for (var i = 0, len = self.segments.length; i < len; i++) {
+        callback(self.segments[i]);
+      }
+    };
+
+    self.init();
 
     return self;
   };
@@ -48,38 +87,43 @@ var SnakeGame = {};
   var Controller = function() {
     var self = {},
         stage = app.stage,
-        segment = app.segment,
+        snake = app.snake,
         direction = config.direction;
 
     self.init = function() {
-      $(window).on('keydown', self.keyHandler);
+      $(window).on('keydown', self.changeDirection);
+      self.center(snake);
     };
-
-    // self.advance = function() {
-    //   self.move();
-    // };
 
     self.move = function() {
       switch (direction) {
         case 'left':
-          segment.position[0] -= 1;
+          snake.position[0] -= 1;
           break;
         case 'right':
-          segment.position[0] += 1;
+          snake.position[0] += 1;
           break;
         case 'up':
-          segment.position[1] -= 1;
+          snake.position[1] -= 1;
           break;
         case 'down':
-          segment.position[1] += 1;
+          snake.position[1] += 1;
           break;
       }
 
       self.checkBorder();
+      snake.advance();
       app.renderer.draw();
     };
 
-    self.keyHandler = function(event) {
+    self.center = function(sprite) {
+      sprite.position = [
+        stage.size[0]/2,
+        stage.size[1]/2
+      ];
+    };
+
+    self.changeDirection = function(event) {
       switch (event.keyCode) {
         case 37: // left
         case 65: // a
@@ -100,22 +144,26 @@ var SnakeGame = {};
         case 32: // space
           direction = 'none';
           break;
+        case 27: // esc
+          app.timer.pause();
+          break;
       }
+      // console.log('key', event.keyCode);
     };
 
     self.checkBorder = function() {
       // left / right
-      if (segment.position[0] < 0) {
-        segment.position[0] = 0;
-      } else if (segment.position[0] >= stage.size[0]) {
-        segment.position[0] = stage.size[0] - 1;
+      if (snake.position[0] < 0) {
+        snake.position[0] = stage.size[0] -1;
+      } else if (snake.position[0] >= stage.size[0]) {
+        snake.position[0] = 0;
       }
 
       // up / down
-      if (segment.position[1] < 0) {
-        segment.position[1] = 0;
-      } else if (segment.position[1] >= stage.size[1]) {
-        segment.position[1] = stage.size[1] - 1;
+      if (snake.position[1] < 0) {
+        snake.position[1] = stage.size[1] -1;
+      } else if (snake.position[1] >= stage.size[1]) {
+        snake.position[1] = 0;
       }
     };
 
@@ -124,63 +172,79 @@ var SnakeGame = {};
     return self;
   };
 
-
   var Timer = function() {
-    self = {};
+    var self = {},
+        frameRate = 1000 / config.fps,
+        timer;
 
     self.init = function() {
-      window.setInterval(self.frame, config.frameRate);
+      self.start();
     };
 
     self.frame = function() {
       app.controller.move();
     };
 
+    self.start = function() {
+      timer = setInterval(self.frame, frameRate);
+    };
+
+    self.stop = function() {
+      clearInterval(timer);
+      timer = null;
+    };
+
+    self.pause = function() {
+      if (timer) {
+        self.stop();
+      } else {
+        self.start();
+      }
+    };
+
     self.init();
+
+    return self;
   };
 
   var Renderer = function() {
     var self = {},
-        body = $('body'),
         scale = config.scale,
         stage = app.stage,
-        segment = app.segment;
+        context = stage.context,
+        snake = app.snake;
 
     self.init = function() {
-      self.add(stage);
-      self.center(segment);
-      self.add(segment);
+      self.draw();
     };
 
     self.draw = function() {
-      self.position(segment);
+      // context.save();
+      self.clear();
+      self.snake();
+      // context.restore();
     };
 
-    self.add = function(sprite) {
-      body.append(sprite.el);
-      self.size(sprite);
-      self.position(sprite);
-    };
-
-    self.position = function(sprite) {
-      sprite.el.css({
-        top: (sprite.position[1] * scale) + 'px',
-        left: (sprite.position[0] * scale) + 'px'
+    self.snake = function() {
+      context.fillStyle = 'red';
+      // console.log('frame --------------------------');
+      snake.eachSegment(function(part) {
+        // console.log('part', part);
+        context.fillRect(
+          part[0] * scale,
+          part[1] * scale,
+          snake.size[0] * scale,
+          snake.size[1] * scale
+        );
       });
     };
 
-    self.size = function(sprite) {
-      sprite.el.css({
-        width: sprite.size[0] * scale,
-        height: sprite.size[1] * scale
-      });
-    };
-
-    self.center = function(sprite) {
-      sprite.position = [
-        stage.size[0]/2,
-        stage.size[1]/2
-      ];
+    self.clear = function() {
+      context.fillStyle = 'black';
+      context.fillRect(0, 0,
+        stage.size[0] * scale,
+        stage.size[1] * scale
+      );
     };
 
     self.init();
